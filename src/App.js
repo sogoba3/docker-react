abda0ddac0cca53895884e4364aaf75fb5812738
Bouba <sogoba3@gmail.com>
2023-09-06 21:53:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there, how are you today! My name is Aboubacar Sidiki Sogoba. Good to know
         </p>
         <a
           className="App-link"
